01eb7366dc95cb8eb29f7189824bf3a1c1fb8a98
Yngredh <yngredh.cruz@gmail.com>
2024-05-08 20:27:15 -0300

    removed isAnagram function from array section to hashtable section

--- src/main/java/Exercises/ExerciseArray.java
+++ src/main/java/Exercises/ExerciseArray.java
@@ -6,28 +6,6 @@ import java.util.Map;
 public class ExerciseArray {
 
     /**
-     * [242] LeetCode <p style="color: green;">Easy</p>
-     * <a href="https://leetcode.com/problems/valid-anagram/">Valid Anagram</a>
-     * @TimeComplexity O(n)
-     * @SpaceComplexity O(s + t)
-     */
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        Map<Character, Integer> freqMap = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            freqMap.put(s.charAt(i), freqMap.getOrDefault(s.charAt(i), 0) + 1);
-            freqMap.put(t.charAt(i), freqMap.getOrDefault(t.charAt(i), 0) - 1);
-        }
-
-        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
-            if (entry.getValue() != 0) return false;
-        }
-
-        return true;
-    }
-
-    /**
      * [1] Leetcode <p style="color: green;">Easy</p>
      * <a href="https://leetcode.com/problems/two-sum/">Two Sum</a>
      * @TimeComplexity O(n)
